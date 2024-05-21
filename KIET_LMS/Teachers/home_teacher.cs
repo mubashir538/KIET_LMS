2d94994552af247131ea4c3a0c48bb2fdccff75d
mubashir-techiq <139107752+mubashir-techiq@users.noreply.github.com>
2024-05-22 01:21:04 +0500

    more fixes teacher side

--- KIET_LMS/Teachers/home_teacher.cs
+++ KIET_LMS/Teachers/home_teacher.cs
@@ -34,7 +34,7 @@ namespace KIET_LMS
             DataTable dt3 = f.getCourses();
             for (int i = 0; i < dt3.Rows.Count; i++)
             {
-                string abr = f.getCourseNames(dt3.Rows[i][1].ToString());
+                string abr = f.getCourseNames(dt3.Rows[i][5].ToString());
                 User_Controls.Course1 c1 = new User_Controls.Course1();
                 c1.course.Text = abr.ToString();
                 c1.id.Text = dt3.Rows[i][0].ToString();
